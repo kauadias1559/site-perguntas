fd2a9981babd99a0cd5480b205de24b24ff827a7
kauadias1559 <161466967+kauadias1559@users.noreply.github.com>
2024-10-15 10:45:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'programação',
+    'O que é HTML?',
+    'HTML é uma linguagem de marcação utilizada na construção de páginas na Web'
+)
+criaCartao(
+    'Computador',
+    'O que é um vírus?',
+    'Em informática, um vírus de computador é um software malicioso que é desenvolvido por programadores geralmente inescrupulosos'
+)
+criaCartao(
+    'Computador',
+    'O que é um vírus?',
+    'Em informática, um vírus de computador é um software malicioso que é desenvolvido por programadores geralmente inescrupulosos'
+)
+criaCartao(
+    'Computador',
+    'O que é um vírus?',
+    'Em informática, um vírus de computador é um software malicioso que é desenvolvido por programadores geralmente inescrupulosos'
+)
